83b8b0f37876a7cd94d99f03344794735147f8c5
walker <qinmu911@163.com>
2019-01-24 18:05:03 +0800

    initial all :tada:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'antd/lib/button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Button type="primary">Button</Button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
