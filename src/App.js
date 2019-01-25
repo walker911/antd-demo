1ff450aab0371451c97bf8f69f6b4c144673243c
walker <qinmu911@163.com>
2019-01-25 10:40:48 +0800

    feat(app): add input and button :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from 'antd/lib/button';
+import { Input, Icon } from 'antd';
+import { Button } from 'antd';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+    }
+  }
+
+  emitEmpty = () => {
+    this.userNameInput.focus();
+    this.setState({ username: '' });
+  }
+
+  onChangeUsername = (e) => {
+    this.setState({ username: e.target.value });
+  }
+
   render() {
+    const { username } = this.state;
+    const suffix = username ? <Icon type="close-circle" onClick={this.emitEmpty} /> : null;
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Input
+          placeholder="Enter your username"
+          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          suffix={suffix}
+          value={username}
+          onChange={this.onChangeUsername}
+          ref={node => this.userNameInput = node}
+        />
+        <Input.Password 
+          placeholder="Input password" 
+          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        />
+        <Button type="primary">登录</Button>
       </div>
     );
   }
