499896ae602b05605e75fff67c4a53ba436edf4e
walker911 <qinmu911@163.com>
2019-01-27 22:51:30 +0800

    feat: add proxy

--- src/util/Api.js
+++ src/util/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = "http://localhost:9090";
+// const api = "http://localhost:9090";
 
 export const login = (body) => 
-    axios.post(`${api}/api/v1/user/login`, body)
+    axios.post(`/api/v1/user/login`, body)
